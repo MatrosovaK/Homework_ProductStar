30feacfce4b2b3f971820aaeea0cd4cae354b0a5
MatrosovaK <skatty.ea@gmail.com>
2024-06-13 23:21:41 +0300

    Update HW4v2.java

--- HW4v2.java
+++ HW4v2.java
@@ -1,17 +1,17 @@
 public class HW4v2{
     public static void main(String[] args) {
-        int n = 22;
+        int n = 26;
         int i = 0;
-        System.out.println(i);
+        System.out.print(i + " ");
         for (i = 1; i <= n; i++) {
             if (i % 3 == 0 & i % 5 == 0) {
-                System.out.println("fizzbuzz");
-            } else if (i % 3 == 0 & i % 5 != 0) {
-                System.out.println("fizz");
+                System.out.print("fizzbuzz ");
             } else if (i % 5 == 0 & i % 3 != 0) {
-                System.out.println("buzz");
+                System.out.print("buzz ");
+            } else if (i % 3 == 0 & i % 5 != 0) {
+                System.out.print("fizz ");
             } else {
-                System.out.println(i);
+                System.out.print(i + " ");
             }
         }
     }
